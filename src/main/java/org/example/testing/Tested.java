afa02b1353992963d3010a7918262d65ce5475aa
Byte69 <belikdana85@gmail.com>
2026-03-04 16:56:06 +0300

    Update Tested.java

--- src/main/java/org/example/testing/Tested.java
+++ src/main/java/org/example/testing/Tested.java
@@ -1,4 +1,20 @@
 package org.example.testing;
 
+import org.example.testing.category.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tested {
+    String name="Тестирование";
+    List<Category>categories=new ArrayList<>();
+    int totalPoints=getAllPoints();
+
+    public int getAllPoints(){
+        return 0;
+    }
+
+    public void startCategory(Category category){
+
+    }
 }
