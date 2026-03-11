6d153315f4ba9f67ad7b23d5a323f32ef0531486
Byte69 <belikdana85@gmail.com>
2026-03-11 09:19:03 +0300

    Update Tested.java

--- src/main/java/org/example/testing/Tested.java
+++ src/main/java/org/example/testing/Tested.java
@@ -1,15 +1,22 @@
 package org.example.testing;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.testing.category.Category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tested {
     String name="Тестирование";
     List<Category>categories=new ArrayList<>();
     int totalPoints=getAllPoints();
-
+    Scanner scanner = new Scanner(System.in)
     public int getAllPoints(){
         return 0;
     }
@@ -17,11 +24,12 @@ public class Tested {
     public Category choesCategory(List<Category> categories){
         System.out.println("Выберите категорию вопросов");
         for (int i = 0; i < categories.size(); i++) {
-
+            System.out.println("Категория - "+categories.get(i).getName()+". Номер "+i+1);
         }
-        return null;
+        int numberCategory= scanner.nextInt();
+        return categories.get(numberCategory-1);
     }
     public void startCategory(Category category){
-
+        
     }
 }
